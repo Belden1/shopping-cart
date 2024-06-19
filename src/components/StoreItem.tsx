cda7b3e83f6600cfc4300495ae620fdef47d1558
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-06-19 17:09:43 +0200

    Shopping cart functionality to StoreItem component

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from 'react-bootstrap';
 import { formatCurrency } from '../utilities/formatCurrency';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 type StoreItemProps = {
   id: number;
@@ -9,7 +10,8 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0; // temporary
+  const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart();
+  const quantity = getItemQuantity(id);
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: 'cover' }} />
@@ -20,17 +22,19 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100">+ Add to Cart</Button>
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add to Cart
+            </Button>
           ) : (
             <div className="d-flex align-items-center flex-column" style={{ gap: '0.5rem' }}>
               <div className="d-flex align-items-center justify-content-center" style={{ gap: '0.5rem' }}>
-                <Button>-</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                 <div>
                   <span className="fs-3">{quantity}</span> in cart
                 </div>
-                <Button>+</Button>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button variant="danger" size="sm">
+              <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>
                 Remove
               </Button>
             </div>
